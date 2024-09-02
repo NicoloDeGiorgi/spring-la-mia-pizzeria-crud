9159d8552cc9ef5de8d8b3c9385d6afe96847d70
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-09-02 14:25:56 +0200

    add : depenncy bootstrap and Table

---
+++ la-mia-pizzeria/src/main/java/com/esercizio/pizzeria/Model/Pizza.java
@@ -0,0 +1,100 @@
+package com.esercizio.pizzeria.Model;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name ="DegaPizzas")
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private String img_url;
+	
+	private double price;
+	
+	private LocalDateTime createdAt;
+	
+	private LocalDateTime updatedAt;
+	
+	
+	//GETTER E SETTER
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNome(String name) {
+		this.name = name;
+	}
+	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescriptione(String description) {
+		this.description = description;
+	}
+
+	
+	public String getImg_url() {
+		return img_url;
+	}
+
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+
+	
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double prezzo) {
+		this.price = prezzo;
+	}
+
+	
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
